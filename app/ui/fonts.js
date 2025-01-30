64c42c3ae9fe22d9ceeff61a19032dcd247e6f61
Yahya <yahyahussein670@gmail.com>
2025-01-30 09:54:06 +0300

    add: fonts

---
+++ app/ui/fonts.js
@@ -0,0 +1,11 @@
+import { Nunito_Sans, Merriweather_Sans} from "next/font/google";
+
+export const nunitoSans = Nunito_Sans({
+  variable: "--font-nunito-sans",
+  subsets: ["latin"],
+});
+
+export const merriweatherSans = Merriweather_Sans({
+  variable: "--font-merriweather-sans",
+  subsets: ["latin"],
+});
