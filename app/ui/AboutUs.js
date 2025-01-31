5b70cce5a9b4b9a936dc361bc1e37097d87acbe3
Yahya <yahyahussein670@gmail.com>
2025-01-31 15:25:42 +0300

    fix: minor tweaks

--- app/ui/AboutUs.js
+++ app/ui/AboutUs.js
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function WhoWeAre() {
   return (
     <>
-    <section className="bg-gray-100 bg-gradient-to-b from-gray-100 via-gray-100 to-gray-200 py-12 md:py-16 px-4 sm:px-8 md:px-12">
-      <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center gap-12 lg:gap-36 md:gap-18">
+    <section className="overflow-hidden bg-gray-100 bg-gradient-to-b from-gray-100 via-gray-100 to-gray-200 py-12 md:py-16 px-4 sm:px-8 md:px-12">
+      <div data-aos="fade-left" className="max-w-6xl mx-auto flex flex-col md:flex-row items-center gap-12 lg:gap-36 md:gap-18">
         {/* Image Section */}
         <div className="w-5/6 md:w-1/2 overflow-hidden rounded-tl-[100px]">
           <Image
@@ -27,7 +27,7 @@ export default function WhoWeAre() {
         </div>
       </div>
     </section>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 120"><path fill="#E5E7EB" fill-opacity="1" d="M0,160L48,149.3C96,139,192,117,288,96C384,75,480,53,576,42.7C672,32,768,32,864,64C960,96,1056,160,1152,176C1248,192,1344,160,1392,144L1440,128L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 120"><path fill="#E5E7EB" fillOpacity="1" d="M0,160L48,149.3C96,139,192,117,288,96C384,75,480,53,576,42.7C672,32,768,32,864,64C960,96,1056,160,1152,176C1248,192,1344,160,1392,144L1440,128L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
     {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#CCFBF1" fill-opacity="1" d="M0,32L48,64C96,96,192,160,288,181.3C384,203,480,181,576,160C672,139,768,117,864,106.7C960,96,1056,96,1152,117.3C1248,139,1344,181,1392,202.7L1440,224L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg> */}
     </>
   );
--- app/ui/Navbar.js
+++ app/ui/Navbar.js
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -11,55 +12,69 @@ function Navbar() {
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
   const closeMenu = () => setIsMenuOpen(false);
 
-  const handleScroll = (e, targetId) => {
-    e.preventDefault();
-    const target = document.getElementById(targetId);
-    if (target) {
-      target.scrollIntoView({ behavior: "smooth", block: "start" });
-      closeMenu(); // Close the menu after navigation
-    }
-  };
-
   return (
     <div
       className={`absolute top-0 p-2 w-full z-10 mt-2 transition-all duration-300 ${
-        pathname === "/" ? "bg-transparent" : "bg-white mt-0 relative"
+        pathname === "/" ? "bg-transparent" : "bg-white mt-0 relative pt-0"
       }`}
     >
       <div className="max-w-screen-xl container py-2 mx-auto px-4 md:py-4 flex justify-between items-center">
         {/* Brand Logo */}
-        <a href="#home" onClick={(e) => handleScroll(e, "home")}>
+        <a href={`/`}>
           <Image
             alt="logo"
-            src={pathname === "/" ? "/Green and Blue Minimalist Pharmacy Logo (3).svg" : "/Green and Blue Minimalist Pharmacy Logo (4).svg"}
+            src={
+              pathname === "/"
+                ? "/Green and Blue Minimalist Pharmacy Logo (3).svg"
+                : "/Green and Blue Minimalist Pharmacy Logo (4).svg"
+            }
             width={180}
             height={60}
+            className="w-[130px] h-[60px] sm:w-[100px] sm:h-[60px] md:w-[180px] md:h-[60px]"
           />
         </a>
 
         {/* Desktop Navigation */}
-        <div className={`hidden md:flex lg:space-x-8 space-x-6 text-xs ${
-  pathname === "/" ? "text-white" : "text-slate-800"
-}`}>
-
-          <a href="#home" onClick={(e) => handleScroll(e, "home")}>
-            HOME
-          </a>
-          <a href="/about">ABOUT US</a>
-          <a href="#projects" onClick={(e) => handleScroll(e, "projects")}>
-            HEALTHCARE SOLUTIONS
-          </a>
-          <a href="#contact" onClick={(e) => handleScroll(e, "contact")}>
-            PHARMACY SOLUTIONS
-          </a>
-          <a href="#contact" onClick={(e) => handleScroll(e, "contact")}>
-            CONTACT US
-          </a>
+        <div
+          className={`hidden md:flex lg:space-x-8 space-x-6 text-xs ${
+            pathname === "/" ? "text-white" : "text-slate-800"
+          }`}
+        >
+          {[
+            { name: "HOME", path: "/" },
+            { name: "ABOUT US", path: "/about" },
+            { name: "HEALTHCARE SOLUTIONS", path: "/healthcare-solutions" },
+            { name: "PHARMACY SOLUTIONS", path: "/pharmacy-solutions" },
+            { name: "CONTACT US", path: "/contact" },
+          ].map((link) => (
+            <Link
+              key={link.path}
+              href={link.path}
+              className={`relative transition-all duration-300 ${
+                pathname === link.path ? "text-teal-500" : "hover:text-teal-500"
+              }`}
+            >
+              {link.name}
+              {/* Hover underline */}
+              <span
+                className={`absolute left-0 bottom-[-3px] h-[2px] w-full transition-all duration-300 ${
+                  pathname === link.path
+                    ? "bg-teal-500"
+                    : "bg-transparent hover:bg-teal-500"
+                }`}
+              />
+            </Link>
+          ))}
         </div>
 
         {/* Mobile Menu Button */}
         <div className="md:hidden">
-          <button onClick={toggleMenu} className="text-white focus:outline-none">
+          <button
+            onClick={toggleMenu}
+            className={`focus:outline-none ${
+              pathname === "/" ? "text-white" : "text-teal-600"
+            }`}
+          >
             <svg
               className="w-8 h-8"
               fill="none"
@@ -67,16 +82,30 @@ function Navbar() {
               viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              ></path>
             </svg>
           </button>
         </div>
       </div>
 
+      {/* Mobile Menu */}
       {isMenuOpen && (
-        <div className="fixed top-0 right-0 h-full w-full bg-white z-50 transition-transform duration-300 ease-in-out">
-          <div className="flex flex-col p-6">
-            <button className="self-end mb-4 text-black focus:outline-none" onClick={closeMenu}>
+        <div className="fixed inset-0 z-50 bg-black/60 backdrop-blur-lg transition-opacity duration-300 ease-in-out">
+          <div
+            className={`fixed top-0 right-0 w-4/5 sm:w-2/5 h-full bg-teal-600 shadow-lg transition-transform duration-300 ease-in-out transform ${
+              isMenuOpen ? "translate-x-0" : "translate-x-full"
+            }`}
+          >
+            {/* Close Button */}
+            <button
+              className="absolute top-6 right-5 p-2 text-white hover:text-black transition-colors"
+              onClick={closeMenu}
+            >
               <svg
                 className="w-8 h-8"
                 fill="none"
@@ -84,22 +113,45 @@ function Navbar() {
                 viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                ></path>
               </svg>
             </button>
-            <div className="flex flex-col gap-6 mt-24 items-center">
-              <a className="text-black" href="#home" onClick={(e) => handleScroll(e, "home")}>
-                Home
-              </a>
-              <a className="text-black" href="#about">
-                About
-              </a>
-              <a className="text-black" href="#projects" onClick={(e) => handleScroll(e, "projects")}>
-                Projects
-              </a>
-              <a className="text-black" href="#contact" onClick={(e) => handleScroll(e, "contact")}>
-                Contact Us
-              </a>
+
+            {/* Mobile Navigation Links */}
+            <div className="flex flex-col gap-8 mt-36 pl-8 text-lg font-light">
+              {[
+                { name: "HOME", path: "/" },
+                { name: "ABOUT", path: "/about" },
+                { name: "HEALTHCARE SOLUTION", path: "/healthcare-solutions" },
+                { name: "PHARMACY SOLUTION", path: "/pharmacy-solutions" },
+                { name: "CONTACT US", path: "/contact" },
+              ].map((link) => (
+                <Link
+                  key={link.path}
+                  href={link.path}
+                  className={`relative text-white transition-all duration-300 ${
+                    pathname === link.path
+                      ? "text-gray-300"
+                      : "hover:text-gray-300"
+                  }`}
+                  onClick={closeMenu} // Close menu when a link is clicked
+                >
+                  {link.name}
+                  {/* Hover underline */}
+                  <span
+                    className={`absolute left-0 bottom-[-3px] h-[2px] w-full transition-all duration-300 ${
+                      pathname === link.path
+                        ? "bg-gray-300"
+                        : "bg-transparent hover:bg-gray-300"
+                    }`}
+                  />
+                </Link>
+              ))}
             </div>
           </div>
         </div>
